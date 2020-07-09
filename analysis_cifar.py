16f3b0729a3e94e29c73e6b11da6d16f15f5dd8a
Alessandro <alessandro.dagostino96@gmail.com>
2020-07-09 16:58:26 +0200

    CIFAR analysis sample images seleted

--- analysis_cifar.py
+++ analysis_cifar.py
@@ -1,4 +1,3 @@
-import imageio
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -9,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set_style('darkgrid')
+# sns.set_style('darkgrid')
 
 from metrics import PSNR, SSIM, MIX, SSIM_loss, SSIM_PSNR
 
@@ -25,41 +24,78 @@ if __name__ == '__main__':
   orig = np.load(orig_path)[:size]
   blur = np.load(blur_path)[:size]
 
-  # Loading Test model
-  model_name = 'unetlike_mse_cifar.h5'
-  model_path = os.path.join(cwd, 'models', model_name)
+  # # Loading Test model
+  # model_name = 'unetlike_mse_cifar.h5'
+  # model_path = os.path.join(cwd, 'models', model_name)
+  #
+  # objects = {'PSNR' : PSNR, 'SSIM' : SSIM, 'MIX' : MIX, 'SSIM_loss' : SSIM_loss, 'SSIM_PSNR' : SSIM_PSNR}
+  # model = tf.keras.models.load_model(model_path, custom_objects=objects)
+  #
+  # # Predict the output for CIFAR10
+  # pred    = model.predict(x=blur)
+  # metrics = model.evaluate(x=blur, y=orig)
+  #
+  # psnr_pred_orig = PSNR(tf.Variable(orig), tf.Variable(pred)).numpy()
+  # psnr_blur_orig = PSNR(tf.Variable(orig), tf.Variable(blur)).numpy()
+  #
+  # ssim_pred_orig = SSIM(tf.Variable(orig), tf.Variable(pred)).numpy()
+  # ssim_blur_orig = SSIM(tf.Variable(orig), tf.Variable(blur)).numpy()
+  #
+  # # mask = psnr_blur_orig < 30
+  #
+  # psnr_pred_orig.mean()
+  # psnr_blur_orig.mean()
+  #
+  # psnr_diff = (psnr_pred_orig - psnr_blur_orig)
+  # mask1 = psnr_diff > 0
+  #
+  # ssim_diff = (ssim_pred_orig - ssim_blur_orig)
+  # mask2 = ssim_diff > 0
 
-  objects = {'PSNR' : PSNR, 'SSIM' : SSIM, 'MIX' : MIX, 'SSIM_loss' : SSIM_loss, 'SSIM_PSNR' : SSIM_PSNR}
-  model = tf.keras.models.load_model(model_path, custom_objects=objects)
+#%%
+pred = np.load( os.path.join(cwd, 'data', 'cifar10_unet_pred.npy'))
 
-  # Predict the output for CIFAR10
-  pred    = model.predict(x=blur)
-  metrics = model.evaluate(x=blur, y=orig)
+# idx = ssim_diff.argmax()
+for idx in [6271, 3822, 7014]:
+    fig, ax = plt.subplots(nrows=1, ncols = 3, figsize=(12,8))
+    for a, source in zip(ax, [orig, blur, pred]):
+      a.imshow(source[idx]);
+      a.set_xticks([])
+      a.set_yticks([])
+    plt.tight_layout()
+    plt.savefig(os.path.join(cwd, 'images', f'CIFAR_{idx}.png'), bbox_inches = 'tight')
 
-  psnr_pred_orig = PSNR(tf.Variable(orig), tf.Variable(pred)).numpy()
-  psnr_blur_orig = PSNR(tf.Variable(orig), tf.Variable(blur)).numpy()
+#%%
+    fig, ax = plt.subplots(nrows=1, ncols = 3, figsize=(12,8))
+    for n, (cm, a) in enumerate(zip(['Reds', 'Greens', 'Blues'], ax)):
+      a.imshow((pred[idx]-blur[idx])[:,:,n], cmap = cm);
+      a.set_xticks([])
+      a.set_yticks([])
+    plt.tight_layout()
+    plt.savefig(os.path.join(cwd, 'images', f'channel_var_CIFAR_{idx}.png'), bbox_inches = 'tight')
 
-  ssim_pred_orig = SSIM(tf.Variable(orig), tf.Variable(pred)).numpy()
-  ssim_blur_orig = SSIM(tf.Variable(orig), tf.Variable(blur)).numpy()
+#%%
+# idx = psnr_diff.argmax()
+idx = 7014
+fig, ax = plt.subplots(nrows=1, ncols = 3, figsize=(12,8))
+for a, source in zip(ax, [orig, blur, pred]):
+  a.imshow(source[idx]);
+  a.set_xticks([])
+  a.set_yticks([])
+plt.tight_layout()
+plt.savefig(os.path.join(cwd, 'images', 'best_psnr_CIFAR.png'), bbox_inches = 'tight')
 
-  # mask = psnr_blur_orig < 30
+#%%
+fig, ax = plt.subplots(nrows=1, ncols = 3, figsize=(12,8))
+for n, (cm, a) in enumerate(zip(['Reds', 'Greens', 'Blues'], ax)):
+  a.imshow((pred[idx]-blur[idx])[:,:,n], cmap = cm);
+  a.set_xticks([])
+  a.set_yticks([])
+plt.tight_layout()
+plt.savefig(os.path.join(cwd, 'images', 'channel_var_psnr_CIFAR.png'), bbox_inches = 'tight')
 
-  (pred[idx]-blur[idx]).min()
-  plt.imshow(pred[7014]-blur[7014]);
 
-  psnr_diff = (psnr_pred_orig - psnr_blur_orig)
-  mask1 = psnr_diff > 0
 
-  plt.imshow(orig[mask][651]);
-  plt.imshow(blur[mask][651]);
-  plt.imshow(pred[mask][651]);
 
-  ssim_diff = (ssim_pred_orig - ssim_blur_orig)
-  mask2 = ssim_diff > 0
 
-  mask = mask1 & mask2
-
-  psnr_pred_orig.mean()
-  psnr_blur_orig.mean()
-
-  plt.scatter(ssim_diff[mask], psnr_diff[mask], marker='.');
+# plt.scatter(ssim_diff[mask], psnr_diff[mask], marker='.');
